5500ac55e47201c560a888deedb4263b25df0027
sebusml <seba.marquez.lutfy@gmail.com>
2022-01-09 12:53:52 -0800

    Improve mobile UI by adding left-right padding to main content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,7 +28,7 @@ const Home: NextPage = () => {
   }, []);
 
   return (
-    <main className="antialiased mx-auto max-w-screen-sm pt-8">
+    <main className="antialiased mx-auto max-w-screen-sm pt-8 px-4 md:px-6">
       <h1 className="font-bold font-sans break-normal text-gray-900 pt-6 pb-2 text-3xl md:text-4xl">
         Welcome to our blog
       </h1>
