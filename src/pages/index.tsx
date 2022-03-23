30363113b685bef33a6edd8ecbd464ba1ce44a7a
sebusml <seba.marquez.lutfy@gmail.com>
2022-03-22 22:27:54 -0700

    Adding testimonialish section with hashtags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -285,6 +285,43 @@ export default function Home() {
               ))}
             </dl>
           </div>
+          {/* Hashtags section section */}
+          <div className="pb-16 bg-gradient-to-r from-purple-400 to-pink-300 lg:pb-0 lg:z-10 lg:relative">
+            <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-3 lg:gap-8">
+              <div className="relative lg:-my-8">
+                <div
+                  aria-hidden="true"
+                  className="absolute inset-x-0 top-0 h-1/2 bg-white lg:hidden"
+                />
+                <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:p-0 lg:h-full">
+                  <div className="aspect-w-10 aspect-h-6 overflow-hidden sm:aspect-w-16 sm:aspect-h-7 lg:aspect-none lg:h-full">
+                    <img
+                      className="object-cover lg:h-full lg:w-full"
+                      src="sofa_girl.png"
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="mt-12 lg:m-0 lg:col-span-2 lg:pl-8">
+                <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:px-0 lg:py-20 lg:max-w-none">
+                  <p className="mt-6 text-2xl font-medium text-white">
+                    Why wait to connect with your favourite People?
+                  </p>
+                  <p className="text-base font-medium text-white mt-6">
+                    Now it is easy to join your friends and community with
+                    PeersConnect. <br></br>Connect with the ones who truly
+                    understands and support you.
+                  </p>
+                  <p className="mt-6 text-2xl font-medium text-white">
+                    <span className="text-blue-500">#womenintech </span>
+                    <span className="text-pink-200">#founders </span>
+                    <span className="text-green-300">#creators</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
 
           {/* Feature section with screenshot */}
           <div className="relative bg-gray-50 pt-16 sm:pt-24 lg:pt-32">
