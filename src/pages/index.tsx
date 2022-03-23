3e7d2332a522cbf87742261423676eb777fbd214
sebusml <seba.marquez.lutfy@gmail.com>
2022-03-22 22:40:28 -0700

    Adding FAQ section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,22 @@ const tractionNumbers = [
   { number: "10K+", kpi: "Support hours", color: "pink" },
   { number: "10+", kpi: "Facilitators", color: "green" },
 ];
-const navigation = [{ name: "Posts", href: "posts" }];
+const faqs = [
+  {
+    question: "Online approach to mental health does not sounds serious",
+    answer:
+      "Facilitators are professional mental health specialists. Sessions are designed using scientific and proven methods.",
+  },
+  {
+    question: "Not sure if group approach is for me",
+    answer: "Groups are small and prescreened",
+  },
+  { question: "No time for physical visits", answer: "Everything is online" },
+  {
+    question: "Therapy sessions are expensive",
+    answer: "Affordable than traditional therapy",
+  },
+];
 const features = [
   {
     name: "We bring together 8-10 people",
@@ -319,6 +334,41 @@ export default function Home() {
                     <span className="text-green-300">#creators</span>
                   </p>
                 </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Kind of FAQ section */}
+          <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-20 lg:px-8">
+            <div className="lg:grid lg:grid-cols-3 lg:gap-8">
+              <div>
+                <h2 className="text-3xl font-extrabold text-gray-900">
+                  What holds you back?
+                </h2>
+                <p className="mt-4 text-lg text-gray-500">
+                  Can’t find the answer you’re looking for? Reach out to our{" "}
+                  <a
+                    href="#"
+                    className="font-medium text-indigo-600 hover:text-indigo-500"
+                  >
+                    customer support
+                  </a>{" "}
+                  team.
+                </p>
+              </div>
+              <div className="lg:mt-0 lg:col-span-2">
+                <dl className="space-y-12">
+                  {faqs.map((faq) => (
+                    <div key={faq.question}>
+                      <dt className="text-lg leading-6 font-medium text-gray-900">
+                        {faq.question}
+                      </dt>
+                      <dd className="mt-2 text-base text-gray-500">
+                        {faq.answer}
+                      </dd>
+                    </div>
+                  ))}
+                </dl>
               </div>
             </div>
           </div>
