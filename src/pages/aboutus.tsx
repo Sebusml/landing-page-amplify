817f1f538cfef824751cd5650c79dcf429c531d9
sebusml <seba.marquez.lutfy@gmail.com>
2022-03-23 00:04:04 -0700

    Adding content for AboutUs page

--- src/pages/aboutus.tsx
+++ src/pages/aboutus.tsx
@@ -1,26 +1,10 @@
-/*
-  This example requires Tailwind CSS v2.0+ 
-  
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/typography'),
-    ],
-  }
-  ```
-*/
 export default function AboutUs() {
   return (
-    <div className="py-16 bg-gray-50 overflow-hidden">
+    <div className="py-16 overflow-hidden">
       <div className="max-w-7xl mx-auto px-4 space-y-8 sm:px-6 lg:px-8">
         <div className="text-base max-w-prose mx-auto lg:max-w-none">
           <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
-            Transactions
+            communities
           </h2>
           <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
             What makes us different
@@ -28,41 +12,34 @@ export default function AboutUs() {
         </div>
         <div className="relative z-10 text-base max-w-prose mx-auto lg:max-w-5xl lg:mx-0 lg:pr-72">
           <p className="text-lg text-gray-500">
-            Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum
-            urna sed consectetur neque tristique pellentesque. Blandit amet, sed
-            aenean erat arcu morbi. Cursus faucibus nunc nisl netus morbi vel
-            porttitor vitae ut. Amet vitae fames senectus vitae.
+            O&apos;Peers is community-oriented mental health support groups for
+            women in workplace.
           </p>
         </div>
         <div className="lg:grid lg:grid-cols-2 lg:gap-8 lg:items-start">
           <div className="relative z-10">
             <div className="prose prose-indigo text-gray-500 mx-auto lg:max-w-none">
               <p>
-                Sollicitudin tristique eros erat odio sed vitae, consequat
-                turpis elementum. Lorem nibh vel, eget pretium arcu vitae. Eros
-                eu viverra donec ut volutpat donec laoreet quam urna.
+                Most people struggle with anxiety, imposter syndrome, and
+                burnout at work. So we created a group that helps us overcome
+                negative feelings so we can start feeling happy again! You
+                don&apos;t have to struggle in silence. Get support from peers.
               </p>
-              <ul role="list">
-                <li>Quis elit egestas venenatis mattis dignissim.</li>
-                <li>
-                  Cras cras lobortis vitae vivamus ultricies facilisis tempus.
-                </li>
-                <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
-              </ul>
               <p>
-                Rhoncus nisl, libero egestas diam fermentum dui. At quis
-                tincidunt vel ultricies. Vulputate aliquet velit faucibus
-                semper. Pellentesque in venenatis vestibulum consectetur nibh
-                id. In id ut tempus egestas. Enim sit aliquam nec, a. Morbi enim
-                fermentum lacus in. Viverra.
+                Connect with other Women in Tech, Founders and Creators. One of
+                our priorities is to reach women (#womenintech, #workingmoms,
+                #femalefounders, etc){" "}
+                <span className="font-bold">
+                  before they hit a crisis point
+                </span>
+                .
               </p>
-              <h3>We’re here to help</h3>
               <p>
-                Tincidunt integer commodo, cursus etiam aliquam neque, et.
-                Consectetur pretium in volutpat, diam. Montes, magna cursus
-                nulla feugiat dignissim id lobortis amet. Laoreet sem est
-                phasellus eu proin massa, lectus. Diam rutrum posuere donec
-                ultricies non morbi. Mi a platea auctor mi.
+                Since many people are daunted by the idea of therapy, we
+                organized support groups with peers instead - to focus on
+                specific issues, like their careers, or overall emotional
+                wellbeing. This helps because it makes the service feel more
+                approachable.
               </p>
             </div>
             <div className="mt-10 flex text-base max-w-prose mx-auto lg:max-w-none">
@@ -71,15 +48,7 @@ export default function AboutUs() {
                   href="#"
                   className="w-full flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700"
                 >
-                  Contact sales
-                </a>
-              </div>
-              <div className="rounded-md shadow ml-4">
-                <a
-                  href="#"
-                  className="w-full flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-50"
-                >
-                  Learn more
+                  Connect now
                 </a>
               </div>
             </div>
@@ -120,11 +89,7 @@ export default function AboutUs() {
             </svg>
             <blockquote className="relative bg-white rounded-lg shadow-lg">
               <div className="rounded-t-lg px-6 py-8 sm:px-10 sm:pt-10 sm:pb-8">
-                <img
-                  src="https://tailwindui.com/img/logos/workcation-logo-indigo-600-mark-gray-800-and-indigo-600-text.svg"
-                  alt="Workcation"
-                  className="h-8"
-                />
+                <img src="logo.svg" alt="opeers" className="h-8" />
                 <div className="relative text-lg text-gray-700 font-medium mt-8">
                   <svg
                     className="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 text-gray-200"
@@ -146,15 +111,15 @@ export default function AboutUs() {
                 <div className="relative rounded-full border-2 border-white sm:absolute sm:top-0 sm:transform sm:-translate-y-1/2">
                   <img
                     className="w-12 h-12 sm:w-20 sm:h-20 rounded-full bg-indigo-300"
-                    src="https://images.unsplash.com/photo-1500917293891-ef795e70e1f6?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2.5&w=160&h=160&q=80"
+                    src="ceo.jpeg"
                     alt=""
                   />
                 </div>
                 <span className="relative ml-4 text-indigo-300 font-semibold leading-6 sm:ml-24 sm:pl-1">
                   <p className="text-white font-semibold sm:inline">
-                    Judith Black
+                    Ardak Yeraliyeva
                   </p>{" "}
-                  <p className="sm:inline">CEO at Workcation</p>
+                  <p className="sm:inline">CEO at O&apos;Peers</p>
                 </span>
               </cite>
             </blockquote>
