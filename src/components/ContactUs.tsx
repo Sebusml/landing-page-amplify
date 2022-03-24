40c3fb102a6759b00f260ffff72b57e15ed137e6
sebusml <seba.marquez.lutfy@gmail.com>
2022-03-24 13:53:15 -0700

    Remove phone number field from contact form

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -61,26 +61,13 @@ export default function ContactUs(): ReactElement {
                 />
               </div>
               <div>
-                <label htmlFor="phone" className="sr-only">
-                  Phone
-                </label>
-                <input
-                  type="text"
-                  name="phone"
-                  id="phone"
-                  autoComplete="tel"
-                  className="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
-                  placeholder="Phone"
-                />
-              </div>
-              <div>
                 <label htmlFor="message" className="sr-only">
                   Message
                 </label>
                 <textarea
                   id="message"
                   name="message"
-                  rows={4}
+                  rows={6}
                   className="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-indigo-500 focus:border-indigo-500 border border-gray-300 rounded-md"
                   placeholder="Message"
                   defaultValue={""}
